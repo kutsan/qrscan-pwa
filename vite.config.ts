0f806814475e817386e1ba926470ea94da6ac51a
Kutsan Kaplan <me@kutsan.dev>
2021-02-15 15:23:59 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+export default defineConfig({
+  plugins: [reactRefresh()],
+  server: {
+    https: true,
+    host: '0.0.0.0',
+  },
+})
